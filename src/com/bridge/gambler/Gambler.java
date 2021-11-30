b1469080c7f186673d88164c714ba6681e0df47d
Navneet <navn0306@gmail.com>
2021-12-01 00:27:23 +0530

    ADD: Functionality to bet, which results in win or loose

--- src/com/bridge/gambler/Gambler.java
+++ src/com/bridge/gambler/Gambler.java
@@ -3,6 +3,9 @@ package com.bridge.gambler;
 public class Gambler {
     static int dailyStake = 100;
     static int betInDollar = 1;
+    public static final int win= 1;
+    public static final int loss =2;
+
 
     Gambler(int dailyStake, int betInDollar) {
         this.dailyStake = dailyStake;
@@ -12,6 +15,16 @@ public class Gambler {
 
     public static void main(String[] args) {
 
+        int result =  (int) ((Math.random() * 10 % 2) + 1);
+
+        if (result == win){
+            dailyStake++;
+            System.out.println("Won");
+        }
+        else{
+            dailyStake--;
+            System.out.println("Lost");
+        }
         System.out.println("Total daily Stake is : " + dailyStake);
         System.out.println("Betting Dollar for every game : " + betInDollar);
 
