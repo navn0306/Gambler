740cff6fc5f7ff05255e5011a382a815476522f0
Navneet <navn0306@gmail.com>
2021-11-30 23:29:55 +0530

    ADDED: The welcome message for the program.

---
+++ src/com/bridge/gambler/Gambler.java
@@ -0,0 +1,9 @@
+package com.bridge.gambler;
+
+public class Gambler {
+    public static void main(String[] args) {
+        System.out.println("\nWelcome, This problem simulates a Gambler who starts with a stake and\n" +
+                "bets every game to win or loose some stake. Being a Calculative\n" +
+                "Gambler exits if the Stake reaches a high or a low limit");
+    }
+}
