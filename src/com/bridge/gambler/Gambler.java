a8d127724340ae33cb204cbe793faa81adc082f4
Navneet <navn0306@gmail.com>
2021-11-30 23:35:33 +0530

    ADD: Initializing two variables dailyStake and betInDollar using constructors

--- src/com/bridge/gambler/Gambler.java
+++ src/com/bridge/gambler/Gambler.java
@@ -1,9 +1,19 @@
 package com.bridge.gambler;
 
 public class Gambler {
+    static int dailyStake = 100;
+    static int betInDollar = 1;
+
+    Gambler(int dailyStake, int betInDollar) {
+        this.dailyStake = dailyStake;
+        this.betInDollar = betInDollar;
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("\nWelcome, This problem simulates a Gambler who starts with a stake and\n" +
-                "bets every game to win or loose some stake. Being a Calculative\n" +
-                "Gambler exits if the Stake reaches a high or a low limit");
+
+        System.out.println("Total daily Stake is : " + dailyStake);
+        System.out.println("Betting Dollar for every game : " + betInDollar);
+
     }
 }
