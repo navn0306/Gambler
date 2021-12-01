ed35eda4983cb69cd60447bd6f210e7150ddf78e
Navneet <navn0306@gmail.com>
2021-12-01 22:57:25 +0530

    ADD: Added UC4, where checking for total amount lost or won forplaying 20 days continously.

--- src/com/bridge/gambler/Gambler.java
+++ src/com/bridge/gambler/Gambler.java
@@ -1,36 +1,42 @@
 package com.bridge.gambler;
 
 public class Gambler {
+
     static int dailyStake = 100;
-    static int betInDollar = 1;
+    static int winCount = 0;
+    static int lossCount = 0;
+    static int totalAmountWonForMonth = 0;
+    static int totalAmountLostForMonth = 0;
+
     public static final int win = 1;
-    public static final int loss = 2;
-
-
-    Gambler(int dailyStake, int betInDollar) {
-        this.dailyStake = dailyStake;
-        this.betInDollar = betInDollar;
-
-    }
 
     public static void main(String[] args) {
-
-        while (dailyStake > 50 && dailyStake != 150 ) {
-
-            int result = (int) ((Math.random() * 10 % 2) + 1);
-
-            if (result == win) {
-                dailyStake++;
-                System.out.println("Won");
-            } else {
-                dailyStake--;
-                System.out.println("Lost");
+        for (int day = 1; day <= 20; day++) {
+            dailyStake = 100;
+            //System.out.println("DAY Number " + day);
+            while (dailyStake >= 51 && dailyStake <= 149) {
+                int result = (int) ((Math.random() * 10 % 2) + 1);
+                if (result == win) {
+                    dailyStake++;
+                    winCount++;
+                } else {
+                    dailyStake--;
+                    lossCount++;
+                }
+                if (dailyStake == 150) {
+                    totalAmountWonForMonth += 50;
+                }
+                else if(dailyStake == 50) {
+                    totalAmountLostForMonth -= 50;
+                }
             }
-
+            //System.out.println("Daily Stake " + dailyStake);
         }
-        System.out.println("Total daily Stake is : " + dailyStake);
-        System.out.println("Betting Dollar for every game : " + betInDollar);
-
+        System.out.println("Won times " + winCount);
+        System.out.println("Lost count " + lossCount);
+        System.out.println("Total amount won for 20 Days " + totalAmountWonForMonth);
+        System.out.println("Total amount lost for 20 Days " + totalAmountLostForMonth);
 
     }
+
 }
