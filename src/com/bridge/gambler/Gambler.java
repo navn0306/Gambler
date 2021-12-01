658e28d65b5d9b11de08019d42950f1170e160b3
Navneet <navn0306@gmail.com>
2021-12-02 00:13:36 +0530

    ADD: Usecase5, implementing the count of number of days lost or won in a month and by how much.

--- src/com/bridge/gambler/Gambler.java
+++ src/com/bridge/gambler/Gambler.java
@@ -5,6 +5,8 @@ public class Gambler {
     static int dailyStake = 100;
     static int winCount = 0;
     static int lossCount = 0;
+    static int dayLostCount = 0;
+    static int dayWonCount = 0;
     static int totalAmountWonForMonth = 0;
     static int totalAmountLostForMonth = 0;
 
@@ -13,7 +15,7 @@ public class Gambler {
     public static void main(String[] args) {
         for (int day = 1; day <= 20; day++) {
             dailyStake = 100;
-
+            System.out.println("DAY Number " + day);
             while (dailyStake >= 51 && dailyStake <= 149) {
                 int result = (int) ((Math.random() * 10 % 2) + 1);
                 if (result == win) {
@@ -23,19 +25,23 @@ public class Gambler {
                     dailyStake--;
                     lossCount++;
                 }
+
                 if (dailyStake == 150) {
                     totalAmountWonForMonth += 50;
-                }
-                else if(dailyStake == 50) {
+                    dayWonCount++;
+                } else if (dailyStake == 50) {
                     totalAmountLostForMonth -= 50;
+                    dayLostCount++;
                 }
+                
             }
-           
         }
         System.out.println("Won times " + winCount);
         System.out.println("Lost count " + lossCount);
         System.out.println("Total amount won for 20 Days " + totalAmountWonForMonth);
         System.out.println("Total amount lost for 20 Days " + totalAmountLostForMonth);
+        System.out.println("Number of days lost " + dayLostCount + " by $50.");
+        System.out.println("Number of days won " + dayWonCount + " by $50.");
 
     }
 
